d9482cb6fccf6e894c6cc418a3364b82db9dbe1d
fajar1st <79302878+fajar1st@users.noreply.github.com>
2022-07-26 14:23:34 +0700

    login

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,4 @@
+<form action="/logout" method="post">
+  @csrf
+  <button style="border: 0; background-color: white;" type="submit" class="nav-link active" aria-current="page">Logout</button>
+</form>
